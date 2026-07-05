c0b4e591c8b59cc963cce3ed60144a4202e02c61
MelnCat <melncatuwu@gmail.com>
2026-07-04 20:01:19 -0700

    update

--- src/parser/treeSitter.ts
+++ src/parser/treeSitter.ts
@@ -1,7 +1,7 @@
 import { Language, Parser } from "web-tree-sitter";
-
+import cSharpWasm from "tree-sitter-c-sharp/tree-sitter-c_sharp.wasm?uint8array";
 await Parser.init();
 
 export const parser = new Parser();
-export const CSharp = await Language.load("./node_modules/tree-sitter-c-sharp/tree-sitter-c_sharp.wasm");
+export const CSharp = await Language.load(cSharpWasm);
 parser.setLanguage(CSharp);
---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+declare module "*?uint8array" {
+    declare const result: Uint8Array;
+    export default result;
+}
